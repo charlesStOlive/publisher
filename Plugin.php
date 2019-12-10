207d128fb3f8ddf18e316612746a1f1027cb7aff
charles <charles.stolive@dayonmars.fr>
2019-12-10 11:16:51 +0100

    init plugin

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Waka\Publisher;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Publisher Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Publisher',
+            'description' => 'No description provided yet...',
+            'author'      => 'Waka',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Waka\Publisher\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'waka.publisher.some_permission' => [
+                'tab' => 'Publisher',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'publisher' => [
+                'label'       => 'Publisher',
+                'url'         => Backend::url('waka/publisher/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['waka.publisher.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
